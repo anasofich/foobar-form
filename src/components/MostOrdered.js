d8104634d9aefddf6353101d2c11b213502cbf84
anasofich <castellanos_chopas@hotmail.com>
2022-01-10 16:18:35 +0100

    added correct images and random beer generator

--- src/components/MostOrdered.js
+++ src/components/MostOrdered.js
@@ -1,46 +1,62 @@
 import React from "react";
 
-let beerNumber = "";
-let beerName = "";
-let beerImg = "";
-let background = "";
-
-setNumber();
-
-function setNumber() {
-  beerNumber = Math.floor(Math.random() * 6) + 1;
-  setBeer();
-}
-
-function setBeer() {
-  if (beerNumber === 1) {
-    beerName = "El Hefe";
-    beerImg = "../beer-images/elhefe.png.webp";
-    background = "linear-gradient(180deg, #FDEEBB 0%, rgba(255, 255, 255, 1) 100%)";
-  } else if (beerNumber === 2) {
-    beerName = "Hollaback Lager";
-    beerImg = "../beer-images/hollaback.png.webp";
-    background = "linear-gradient(180deg, #FEE2CD 0%, rgba(255, 255, 255, 1) 100%)";
-  } else if (beerNumber === 3) {
-    beerName = "Hoppily Ever After";
-    beerImg = "../beer-images/hoppilyeverafter.png.webp";
-    background = "linear-gradient(180deg, #DAFCAE 0%, rgba(255, 255, 255, 1) 100%)";
-  } else if (beerNumber === 4) {
-    beerName = "Ruined Childhood";
-    beerImg = "../beer-images/ruinedchildhood.png.webp";
-    background = "linear-gradient(180deg, #FBFDB6 0%, rgba(255, 255, 255, 1) 100%)";
-  } else if (beerNumber === 5) {
-    beerName = "Sleighride";
-    beerImg = "../beer-images/sleighride.png.webp";
-    background = "linear-gradient(180deg, #E0D0FD 0%, rgba(255, 255, 255, 1) 100%)";
-  } else if (beerNumber === 6) {
-    beerName = "Steampunk";
-    beerImg = "../beer-images/steampunk.png.webp";
-    background = "linear-gradient(180deg, #CDDEEB 0%, rgba(255, 255, 255, 1) 100%)";
-  }
-}
-
 export default function MostOrdered(props) {
+  let beerNumber = "";
+  let beerName = "";
+  let beerImg = "";
+  let background = "";
+
+  setNumber();
+
+  function setNumber() {
+    beerNumber = Math.floor(Math.random() * 10) + 1;
+    setBeer();
+  }
+
+  function setBeer() {
+    if (beerNumber === 1) {
+      beerName = "El Hefe";
+      beerImg = "../beer-images/elhefe.png.webp";
+      background = "linear-gradient(180deg, #FDEEBB 0%, rgba(255, 255, 255, 1) 100%)";
+    } else if (beerNumber === 2) {
+      beerName = "Hollaback Lager";
+      beerImg = "../beer-images/hollaback.png.webp";
+      background = "linear-gradient(180deg, #FEE2CD 0%, rgba(255, 255, 255, 1) 100%)";
+    } else if (beerNumber === 3) {
+      beerName = "Hoppily Ever After";
+      beerImg = "../beer-images/hoppilyeverafter.png.webp";
+      background = "linear-gradient(180deg, #DAFCAE 0%, rgba(255, 255, 255, 1) 100%)";
+    } else if (beerNumber === 4) {
+      beerName = "Ruined Childhood";
+      beerImg = "../beer-images/ruinedchildhood.png.webp";
+      background = "linear-gradient(180deg, #FBFDB6 0%, rgba(255, 255, 255, 1) 100%)";
+    } else if (beerNumber === 5) {
+      beerName = "Sleighride";
+      beerImg = "../beer-images/sleighride.png.webp";
+      background = "linear-gradient(180deg, #E0D0FD 0%, rgba(255, 255, 255, 1) 100%)";
+    } else if (beerNumber === 6) {
+      beerName = "Steampunk";
+      beerImg = "../beer-images/steampunk.png.webp";
+      background = "linear-gradient(180deg, #CDDEEB 0%, rgba(255, 255, 255, 1) 100%)";
+    } else if (beerNumber === 7) {
+      beerName = "Fairy Tale Ale";
+      beerImg = "../beer-images/fairytaleale.png.webp";
+      background = "linear-gradient(180deg, #F9E1FF 0%, rgba(255, 255, 255, 1) 100%)";
+    } else if (beerNumber === 8) {
+      beerName = "Row 26";
+      beerImg = "../beer-images/row26.png.webp";
+      background = "linear-gradient(180deg, #C7D3FA 0%, rgba(255, 255, 255, 1) 100%)";
+    } else if (beerNumber === 9) {
+      beerName = "GitHop";
+      beerImg = "../beer-images/githop.png.webp";
+      background = "linear-gradient(180deg, #FDD1D0 0%, rgba(255, 255, 255, 1) 100%)";
+    } else if (beerNumber === 10) {
+      beerName = "Mowintime";
+      beerImg = "../beer-images/mowintime.png.webp";
+      background = "linear-gradient(180deg, #B8FCE1 0%, rgba(255, 255, 255, 1) 100%)";
+    }
+  }
+
   return (
     <div className="beerSugItem">
       <h2>Most ordered today</h2>
--- src/components/RandomBeer.js
+++ src/components/RandomBeer.js
@@ -1,48 +1,156 @@
-import React from "react";
-
-let beerNumber = "";
-let beerName = "";
-let beerImg = "";
-let background = "";
-
-setNumber();
-
-function setNumber() {
-  beerNumber = Math.floor(Math.random() * 6) + 1;
-  console.log(beerNumber);
-  setBeer();
-}
-
-function setBeer() {
-  console.log("setBeer");
-  if (beerNumber === 1) {
-    beerName = "El Hefe";
-    beerImg = "../beer-images/elhefe.png.webp";
-    background = "linear-gradient(180deg, #FDEEBB 0%, rgba(255, 255, 255, 1) 100%)";
-  } else if (beerNumber === 2) {
-    beerName = "Hollaback Lager";
-    beerImg = "../beer-images/hollaback.png.webp";
-    background = "linear-gradient(180deg, #FEE2CD 0%, rgba(255, 255, 255, 1) 100%)";
-  } else if (beerNumber === 3) {
-    beerName = "Hoppily Ever After";
-    beerImg = "../beer-images/hoppilyeverafter.png.webp";
-    background = "linear-gradient(180deg, #DAFCAE 0%, rgba(255, 255, 255, 1) 100%)";
-  } else if (beerNumber === 4) {
-    beerName = "Ruined Childhood";
-    beerImg = "../beer-images/ruinedchildhood.png.webp";
-    background = "linear-gradient(180deg, #FBFDB6 0%, rgba(255, 255, 255, 1) 100%)";
-  } else if (beerNumber === 5) {
-    beerName = "Sleighride";
-    beerImg = "../beer-images/sleighride.png.webp";
-    background = "linear-gradient(180deg, #E0D0FD 0%, rgba(255, 255, 255, 1) 100%)";
-  } else if (beerNumber === 6) {
-    beerName = "Steampunk";
-    beerImg = "../beer-images/steampunk.png.webp";
-    background = "linear-gradient(180deg, #CDDEEB 0%, rgba(255, 255, 255, 1) 100%)";
-  }
-}
+import React, { useState } from "react";
 
 export default function RandomBeer(props) {
+  //for showing initial beer generating a random number and assigning a differnt beer to each number
+
+  /*   setNumber();
+
+  function setNumber() {
+    beerNumber = Math.floor(Math.random() * 10) + 1;
+    console.log(beerNumber);
+    setBeer();
+  }
+
+  function setBeer() {
+    if (beerNumber === 1) {
+      beerName = "El Hefe";
+      beerImg = "../beer-images/elhefe.png.webp";
+      background = "linear-gradient(180deg, #FDEEBB 0%, rgba(255, 255, 255, 1) 100%)";
+    } else if (beerNumber === 2) {
+      beerName = "Hollaback Lager";
+      beerImg = "../beer-images/hollaback.png.webp";
+      background = "linear-gradient(180deg, #FEE2CD 0%, rgba(255, 255, 255, 1) 100%)";
+    } else if (beerNumber === 3) {
+      beerName = "Hoppily Ever After";
+      beerImg = "../beer-images/hoppilyeverafter.png.webp";
+      background = "linear-gradient(180deg, #DAFCAE 0%, rgba(255, 255, 255, 1) 100%)";
+    } else if (beerNumber === 4) {
+      beerName = "Ruined Childhood";
+      beerImg = "../beer-images/ruinedchildhood.png.webp";
+      background = "linear-gradient(180deg, #FBFDB6 0%, rgba(255, 255, 255, 1) 100%)";
+    } else if (beerNumber === 5) {
+      beerName = "Sleighride";
+      beerImg = "../beer-images/sleighride.png.webp";
+      background = "linear-gradient(180deg, #E0D0FD 0%, rgba(255, 255, 255, 1) 100%)";
+    } else if (beerNumber === 6) {
+      beerName = "Steampunk";
+      beerImg = "../beer-images/steampunk.png.webp";
+      background = "linear-gradient(180deg, #CDDEEB 0%, rgba(255, 255, 255, 1) 100%)";
+    } else if (beerNumber === 7) {
+      beerName = "Fairy Tale Ale";
+      beerImg = "../beer-images/fairytaleale.png.webp";
+      background = "linear-gradient(180deg, #F9E1FF 0%, rgba(255, 255, 255, 1) 100%)";
+    } else if (beerNumber === 8) {
+      beerName = "Row 26";
+      beerImg = "../beer-images/row26.png.webp";
+      background = "linear-gradient(180deg, #C7D3FA 0%, rgba(255, 255, 255, 1) 100%)";
+    } else if (beerNumber === 9) {
+      beerName = "GitHop";
+      beerImg = "../beer-images/githop.png.webp";
+      background = "linear-gradient(180deg, #FDD1D0 0%, rgba(255, 255, 255, 1) 100%)";
+    } else if (beerNumber === 10) {
+      beerName = "Mowintime";
+      beerImg = "../beer-images/mowintime.png.webp";
+      background = "linear-gradient(180deg, #B8FCE1 0%, rgba(255, 255, 255, 1) 100%)";
+    }
+  } */
+
+  //for updating the random beer when clicking on button
+  const [beerNumber, setNumber] = useState(1);
+  const [beerName, setBeerName] = useState("Mowintime");
+  const [beerImg, setImg] = useState("../beer-images/mowintime.png.webp");
+  const [background, setBackground] = useState("linear-gradient(180deg, #B8FCE1 0%, rgba(255, 255, 255, 1) 100%)");
+
+  function setNewNumber() {
+    const newNumber = Math.floor(Math.random() * 10) + 1;
+    setNumber(newNumber);
+    setNewBeer();
+  }
+
+  function setNewBeer() {
+    if (beerNumber === 1) {
+      const newName = "El Hefe";
+      const newImg = "../beer-images/elhefe.png.webp";
+      const newBackground = "linear-gradient(180deg, #FDEEBB 0%, rgba(255, 255, 255, 1) 100%)";
+
+      setBeerName(newName);
+      setImg(newImg);
+      setBackground(newBackground);
+    } else if (beerNumber === 2) {
+      const newName = "Hollaback Lager";
+      const newImg = "../beer-images/hollaback.png.webp";
+      const newBackground = "linear-gradient(180deg, #FEE2CD 0%, rgba(255, 255, 255, 1) 100%)";
+
+      setBeerName(newName);
+      setImg(newImg);
+      setBackground(newBackground);
+    } else if (beerNumber === 3) {
+      const newName = "Hoppily Ever After";
+      const newImg = "../beer-images/hoppilyeverafter.png.webp";
+      const newBackground = "linear-gradient(180deg, #DAFCAE 0%, rgba(255, 255, 255, 1) 100%)";
+
+      setBeerName(newName);
+      setImg(newImg);
+      setBackground(newBackground);
+    } else if (beerNumber === 4) {
+      const newName = "Ruined Childhood";
+      const newImg = "../beer-images/ruinedchildhood.png.webp";
+      const newBackground = "linear-gradient(180deg, #FBFDB6 0%, rgba(255, 255, 255, 1) 100%)";
+
+      setBeerName(newName);
+      setImg(newImg);
+      setBackground(newBackground);
+    } else if (beerNumber === 5) {
+      const newName = "Sleighride";
+      const newImg = "../beer-images/sleighride.png.webp";
+      const newBackground = "linear-gradient(180deg, #E0D0FD 0%, rgba(255, 255, 255, 1) 100%)";
+
+      setBeerName(newName);
+      setImg(newImg);
+      setBackground(newBackground);
+    } else if (beerNumber === 6) {
+      const newName = "Steampunk";
+      const newImg = "../beer-images/steampunk.png.webp";
+      const newBackground = "linear-gradient(180deg, #CDDEEB 0%, rgba(255, 255, 255, 1) 100%)";
+
+      setBeerName(newName);
+      setImg(newImg);
+      setBackground(newBackground);
+    } else if (beerNumber === 7) {
+      const newName = "Fairy Tale Ale";
+      const newImg = "../beer-images/fairytaleale.png.webp";
+      const newBackground = "linear-gradient(180deg, #F9E1FF 0%, rgba(255, 255, 255, 1) 100%)";
+
+      setBeerName(newName);
+      setImg(newImg);
+      setBackground(newBackground);
+    } else if (beerNumber === 8) {
+      const newName = "Row 26";
+      const newImg = "../beer-images/row26.png.webp";
+      const newBackground = "linear-gradient(180deg, #C7D3FA 0%, rgba(255, 255, 255, 1) 100%)";
+
+      setBeerName(newName);
+      setImg(newImg);
+      setBackground(newBackground);
+    } else if (beerNumber === 9) {
+      const newName = "GitHop";
+      const newImg = "../beer-images/githop.png.webp";
+      const newBackground = "linear-gradient(180deg, #FDD1D0 0%, rgba(255, 255, 255, 1) 100%)";
+
+      setBeerName(newName);
+      setImg(newImg);
+      setBackground(newBackground);
+    } else if (beerNumber === 10) {
+      const newName = "Mowintime";
+      const newImg = "../beer-images/mowintime.png.webp";
+      const newBackground = "linear-gradient(180deg, #B8FCE1 0%, rgba(255, 255, 255, 1) 100%)";
+
+      setBeerName(newName);
+      setImg(newImg);
+      setBackground(newBackground);
+    }
+  }
+
   return (
     <div className="beerSugItem">
       <h2>Get a random beer!</h2>
@@ -55,7 +163,7 @@ export default function RandomBeer(props) {
         <img className="popularImg" src={beerImg} />
       </div>
       <h1>{beerName}</h1>
-      <button onClick={setNumber}>Change beer</button>
+      <button onClick={setNewNumber}>Change beer</button>
     </div>
   );
 }
